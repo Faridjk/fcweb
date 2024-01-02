406ce19b7aa3b21583eac0a8d3266c50f9b02207
farid <gddg@gmail.com>
2024-01-02 19:16:37 +0400

    added animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
 $(function () {
-
-    $('.navbar-toggler > img').click(function() {
-        if ($('.navbar-toggler > img').attr('src') == 'style/navbar.ico') {
-            $('.navbar-toggler > img').attr('src') == 'style/close.png';
-        } else{
-            $('.navbar-toggler > img').attr('src') == 'style/navbar.ico';
-        }
-    });
+    $('nav').fadeIn(1000)
 });
